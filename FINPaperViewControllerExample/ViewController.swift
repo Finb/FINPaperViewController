1c0bac65c764b008d40929c83fca9435a7e7569e
黄丰 <huangfeng@gozap.com>
2016-06-22 13:07:45 +0800

    init

---
+++ FINPaperViewControllerExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FINPaperViewControllerExample
+//
+//  Created by huangfeng on 16/6/22.
+//  Copyright © 2016年 Fin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
